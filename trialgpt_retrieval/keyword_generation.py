6822b3f20fab4847ebe31362584943bbace3e110
J-Nanshi <jnanshi@genesilico.ai>
2024-07-29 16:40:16 +0530

    custom changes to keyword generation

--- trialgpt_retrieval/keyword_generation.py
+++ trialgpt_retrieval/keyword_generation.py
@@ -3,19 +3,24 @@ __author__ = "qiao"
 """
 generate the search keywords for each patient
 """
-
+#%%
+# !pip install openai
+#%%
+import openai
 import json
 import os
-from openai import AzureOpenAI
-
+from openai import OpenAI
 import sys
-
-client = AzureOpenAI(
-	api_version="2023-09-01-preview",
-	azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
-	api_key=os.getenv("OPENAI_API_KEY"),
+#%%
+from configparser import ConfigParser
+config = ConfigParser() 
+config.read('../secrets.ini')
+# %%
+os.environ["OPENAI_API_KEY"] = config['OpenAI.Science-vNext-Internal']['api_key']
+client = openai.OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
 )
-
 
 def get_keyword_generation_messages(note):
 	system = 'You are a helpful assistant and your task is to help search relevant clinical trials for a given patient description. Please first summarize the main medical problems of the patient. Then generate up to 32 key conditions for searching relevant clinical trials for this patient. The key condition list should be ranked by priority. Please output only a JSON dict formatted as Dict{{"summary": Str(summary), "conditions": List[Str(condition)]}}.'
@@ -28,32 +33,37 @@ def get_keyword_generation_messages(note):
 	]
 	
 	return messages
+#%%
 
+# the corpus: trec_2021, trec_2022, or sigir
+#%%
+corpus = "sigir"
+#%%
+# the model index to use
+model = "gpt-4-turbo"
+#%%
+outputs = {}
 
-if __name__ == "__main__":
-	# the corpus: trec_2021, trec_2022, or sigir
-	corpus = sys.argv[1]
+with open(r"D:\Job\TrialGPT\dataset\sigir\queries.jsonl", "r") as f:
+	for line in f.readlines():
+		entry = json.loads(line)
+		print(f"the json file query {entry}")
+		messages = get_keyword_generation_messages(entry["text"])
 
-	# the model index to use
-	model = sys.argv[2]
+		response = client.chat.completions.create(
+			model=model,
+			messages=messages,
+			temperature=0,
+		)
 
-	outputs = {}
-	
-	with open(f"dataset/{corpus}/queries.jsonl", "r") as f:
-		for line in f.readlines():
-			entry = json.loads(line)
-			messages = get_keyword_generation_messages(entry["text"])
+		output = response.choices[0].message.content
+		print(f"response from LLM,\n{output}")
+		output = output.strip("`").strip("json")
+		
+		outputs[entry["_id"]] = json.loads(output)
+		print(f'''the outputs[entry["_id"]] is\n {outputs[entry["_id"]]}''' )
 
-			response = client.chat.completions.create(
-				model=model,
-				messages=messages,
-				temperature=0,
-			)
-
-			output = response.choices[0].message.content
-			output = output.strip("`").strip("json")
-			
-			outputs[entry["_id"]] = json.loads(output)
-
-			with open(f"results/retrieval_keywords_{model}_{corpus}.json", "w") as f:
-				json.dump(outputs, f, indent=4)
+		with open(r"D:\Job\TrialGPT\results\retrieval_keywords_gpt4turbo_sigir.json", "w") as f:
+			json.dump(outputs, f, indent=4)
+		break
+# %%
